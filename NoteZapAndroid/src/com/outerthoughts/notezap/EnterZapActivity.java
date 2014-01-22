7ce2e989003a5f32715a74d4dd51a235d1d6e66e
Alexandre Rafalovitch <arafalov@gmail.com>
2014-01-22 23:04:18 +0700

    Catch Send click event.

--- NoteZapAndroid/src/com/outerthoughts/notezap/EnterZapActivity.java
+++ NoteZapAndroid/src/com/outerthoughts/notezap/EnterZapActivity.java
@@ -2,9 +2,15 @@ package com.outerthoughts.notezap;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.sax.TextElementListener;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EnterZapActivity extends Activity {
 
@@ -14,6 +20,17 @@ public class EnterZapActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
 		setContentView(R.layout.activity_enter_zap);
+		Button zapSend = (Button)findViewById(R.id.zapSend);
+		zapSend.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				EditText text = ((EditText)findViewById(R.id.zapEditText));
+				
+				Log.i("NoteZap", "Button is clicked with text: " + 
+						text.getText().toString());
+				text.setText("Updated");
+			}
+		});
 	}
 
 	@Override
