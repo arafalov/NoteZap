6d7b7a8b55b4878dff171d994859a619b1ca9837
Alexandre Rafalovitch <arafalov@gmail.com>
2014-01-23 22:54:00 +0700

    Cleaned up code and imports a bit

--- NoteZapAndroid/src/com/outerthoughts/notezap/EnterZapActivity.java
+++ NoteZapAndroid/src/com/outerthoughts/notezap/EnterZapActivity.java
@@ -1,19 +1,15 @@
 package com.outerthoughts.notezap;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.sax.TextElementListener;
-import android.support.v4.app.NotificationCompat;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,9 +33,10 @@ public class EnterZapActivity extends Activity {
 						text.getText().toString());
 
 				Context context = getApplicationContext();
-				NotificationCompat.Builder mBuilder =
-						new NotificationCompat.Builder(context)
-							.setContentInfo("1")
+				Notification.Builder mBuilder =
+						new Notification.Builder(context)
+							.setNumber(1)
+							.setAutoCancel(true)
 							.setSmallIcon(R.drawable.ic_launcher)
 							.setContentTitle("Sending Zap")
 							.setContentText(text.getText().toString());
@@ -56,7 +53,6 @@ public class EnterZapActivity extends Activity {
 				Toast toast = Toast.makeText(context, "Button pressed: " + text.getText(), Toast.LENGTH_SHORT);
 				toast.show();
 				finish();
-//				text.setText("Updated");
 			}
 		});
 	}
