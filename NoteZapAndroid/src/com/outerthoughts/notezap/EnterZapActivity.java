19eb209c36d50dfa5689e134ed416552137dd9f7
Alexandre Rafalovitch <arafalov@gmail.com>
2014-01-23 22:43:43 +0700

    OnClick do a notification icon and a toast (for debug purposes)

--- NoteZapAndroid/src/com/outerthoughts/notezap/EnterZapActivity.java
+++ NoteZapAndroid/src/com/outerthoughts/notezap/EnterZapActivity.java
@@ -2,7 +2,13 @@ package com.outerthoughts.notezap;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.sax.TextElementListener;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -11,14 +17,15 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EnterZapActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+//		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 		setContentView(R.layout.activity_enter_zap);
 		Button zapSend = (Button)findViewById(R.id.zapSend);
 		zapSend.setOnClickListener(new OnClickListener() {			
@@ -28,16 +35,37 @@ public class EnterZapActivity extends Activity {
 				
 				Log.i("NoteZap", "Button is clicked with text: " + 
 						text.getText().toString());
-				text.setText("Updated");
+
+				Context context = getApplicationContext();
+				NotificationCompat.Builder mBuilder =
+						new NotificationCompat.Builder(context)
+							.setContentInfo("1")
+							.setSmallIcon(R.drawable.ic_launcher)
+							.setContentTitle("Sending Zap")
+							.setContentText(text.getText().toString());
+				Intent notifyIntent = new Intent(context, PreferencesMainActivity.class);
+				notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+				PendingIntent pIntent = PendingIntent.getActivity(context, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+				mBuilder.setContentIntent(pIntent);
+				NotificationManager mNotificationManager =
+						(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+				mNotificationManager.notify(1, mBuilder.build());
+				Log.i("NoteZap", "Notifiction setup");
+				
+				
+				Toast toast = Toast.makeText(context, "Button pressed: " + text.getText(), Toast.LENGTH_SHORT);
+				toast.show();
+				finish();
+//				text.setText("Updated");
 			}
 		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.enter_zap, menu);
-		return true;
-	}
+//		getMenuInflater().inflate(R.menu.enter_zap, menu);
+//		return true;
+//	}
 
 }
