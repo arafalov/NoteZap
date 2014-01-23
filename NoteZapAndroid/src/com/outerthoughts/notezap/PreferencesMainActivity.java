4bf0f157bc2619f89da71ccaf5d2521dde425d5c
Alexandre Rafalovitch <arafalov@gmail.com>
2014-01-23 18:35:14 +0700

    Rename old confusing activity/layout to be start of main preference screen

--- NoteZapAndroid/src/com/outerthoughts/notezap/PreferencesMainActivity.java
+++ NoteZapAndroid/src/com/outerthoughts/notezap/PreferencesMainActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class WidgetActivity extends Activity {
+public class PreferencesMainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_widget);
+		setContentView(R.layout.activity_preferences_main);
 	}
 
 	@Override
