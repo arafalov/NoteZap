c4324801b6b70375a348addc0120219ce05a00f4
Alexandre Rafalovitch <arafalov@gmail.com>
2014-01-25 14:45:44 +0700

    Add basic connectivity check

--- NoteZapAndroid/src/com/outerthoughts/notezap/SendIntentService.java
+++ NoteZapAndroid/src/com/outerthoughts/notezap/SendIntentService.java
@@ -1,7 +1,6 @@
 package com.outerthoughts.notezap;
 
 import java.io.IOException;
-import java.util.LinkedList;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -13,7 +12,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 public class SendIntentService extends IntentService {
@@ -34,28 +36,54 @@ public class SendIntentService extends IntentService {
 		String zap = intent.getStringExtra(PARAM_IN_ZAP_TEXT);
 //		queue.push(zap);
 		
-	      HttpClient myClient = new DefaultHttpClient();
-	      HttpGet myConnection = new HttpGet(ZAP_ENDPOINT + "?zap=" + zap);
-	         
-	      String result = null;
-	      JSONObject json = null;
-	      try {
-	            HttpResponse response = myClient.execute(myConnection);
-	            result = EntityUtils.toString(response.getEntity(), "UTF-8");
-	             
-	        } catch (ClientProtocolException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	         
-	         
-	        try{
-	            json = new JSONObject(result);
-	            Log.i("ZAP result json: ", json.toString(2)); 	             
-	        } catch ( JSONException e) {
-	            e.printStackTrace();                
-	        }
+		if (isNetworkAvailable())
+		{
+			//catch failure to send
+			sendZapNow(zap);
+		}
+		else
+		{
+			Log.i("NoteZap", "Network is not avaible. Wait for notification");
+			//TODO: create broadcast listener, if possible?
+		}
+		
 	}
+	
+	private void sendZapNow(String zap) {
+		HttpClient myClient = new DefaultHttpClient();
+		HttpGet myConnection = new HttpGet(ZAP_ENDPOINT + "?zap=" + zap);
+
+		String result = null;
+		JSONObject json = null;
+		try {
+			HttpResponse response = myClient.execute(myConnection);
+			result = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+		try{
+			json = new JSONObject(result);
+			Log.i("ZAP result json: ", json.toString(2)); 	             
+		} catch ( JSONException e) {
+			e.printStackTrace();                
+		}
+	}
+
+	private boolean isNetworkAvailable() {
+	    ConnectivityManager cm = (ConnectivityManager) 
+	      getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+	    // if no network is available networkInfo will be null
+	    // otherwise check if we are connected
+	    if (networkInfo != null && networkInfo.isConnected()) {
+	        return true;
+	    }
+	    return false;
+	} 
 
 }
