6404b18eb55fb4035289b9e150b73203f582c752
Alexandre Rafalovitch <arafalov@gmail.com>
2014-01-25 17:04:53 +0700

    Register for network status notifications

---
+++ NoteZapAndroid/src/com/outerthoughts/notezap/NetworkStateChangeReceiver.java
@@ -0,0 +1,30 @@
+package com.outerthoughts.notezap;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public class NetworkStateChangeReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.i("NoteZap", "Network state changed. Thread id: " + Thread.currentThread().getId() + ". Network is: " + (isNetworkAvailable(context)?"available":"not available"));
+
+	}
+
+	private boolean isNetworkAvailable(Context context) {
+	    ConnectivityManager cm = (ConnectivityManager) 
+	      context.getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+	    // if no network is available networkInfo will be null
+	    // otherwise check if we are connected
+	    if (networkInfo != null && networkInfo.isConnected()) {
+	        return true;
+	    }
+	    return false;
+	} 
+
+}
